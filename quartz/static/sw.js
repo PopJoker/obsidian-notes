54908e2c5c159891d9c45430ab1850393336318c
maverick <maverick.tu@gus-tech.com.tw>
2026-04-28 17:54:15 +0800

    fix: upgrade node version to 22 for quartz

--- quartz/static/sw.js
+++ quartz/static/sw.js
@@ -1,10 +1,49 @@
-// 監聽 fetch 事件（這是 PWA 判定必備）
-self.addEventListener('fetch', function(event) {
-  // 這裡不寫邏輯，直接讓請求通過
+const CACHE_NAME = 'quartz-pwa-v1';
+// 你可以列出想要離線訪問的關鍵路徑
+const PRE_CACHE = [
+  './',
+  './index.html',
+  './static/icon.png',
+  './index.css'
+];
+
+// 安裝時預抓取資源
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(PRE_CACHE))
+      .then(self.skipWaiting())
+  );
 });
 
-// 監聽安裝事件
-self.addEventListener('install', function(event) {
-  console.log('SW installed');
-  self.skipWaiting();
+// 激活時清理舊快取
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
+// 核心：策略改為「網路優先，失敗則回傳快取」
+// 這樣可以確保在有網時看最新內容，沒網時看舊內容
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // 如果請求成功，把內容存入快取
+        const resClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, resClone);
+        });
+        return response;
+      })
+      .catch(() => {
+        // 如果網路斷了，從快取撈資料
+        return caches.match(event.request);
+      })
+  );
 });
