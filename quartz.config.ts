79040a215d91e4cda580516e2f27346681229275
maverick <maverick.tu@gus-tech.com.tw>
2026-04-28 16:50:20 +0800

    fix: upgrade node version to 22 for quartz

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "popJoker.github.io/obsidian-notes",
+    baseUrl: "PopJoker.github.io/obsidian-notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
